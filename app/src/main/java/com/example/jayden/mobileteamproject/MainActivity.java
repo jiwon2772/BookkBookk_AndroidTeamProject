a5075eb0c5e2ce6cc433abfec382907580669427
Jiwon <jiwon2772@gmail.com>
2016-05-06 12:47:45 +0900

    first commmit in develop branch

--- app/src/main/java/com/example/jayden/mobileteamproject/MainActivity.java
+++ app/src/main/java/com/example/jayden/mobileteamproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jayden.mobileteamproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// from 2016/05/06 by Jiwon
 public class MainActivity extends AppCompatActivity {
 
     @Override
