604cc09b5851afcc7f97ac729b1aa1a99244d55b
Jiwon <jiwon2772@gmail.com>
2016-06-10 13:54:30 +0900

    adding splash activity

---
+++ app/src/main/java/com/example/jayden/mobileteamproject/Main/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.example.jayden.mobileteamproject.Main;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.jayden.mobileteamproject.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static String TAG = SplashActivity.class.getName();
+    private static long SLEEP_TIME = 2; // Sleep for some time
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Removes title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Removes notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        // Start timer and launch main activity
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
+    }
+
+    private class IntentLauncher extends Thread {
+        @Override
+        /**
+         * Sleep for some time and than start new activity.
+         */
+        public void run() {
+            try {
+                // Sleeping
+                Thread.sleep(SLEEP_TIME * 1000);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+            // Start main activity
+            Intent intent = new Intent(SplashActivity.this,
+                   LoginActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+}
